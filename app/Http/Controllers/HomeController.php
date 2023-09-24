2df15c1a6f916b76ed8f8fb00751b068f54887b0
AnnyXi <114316992+Anny-Xi@users.noreply.github.com>
2023-09-24 15:05:43 +0200

    Create migration
    Create migration for cubes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index()//$name
+    {
+        $todayDate = date("d.m.Y");
+        return view('home', compact('todayDate'));//,'name'
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,8 @@
 
 @section('content')
 
-    <h1>Welkom bij Rubiks Cub pagina</h1>
+    <h1>Welkom bij Rubiks Cub pagina </h1> {{--    {{$name}}--}}
+    <h1>Vandaag is {{$todayDate}}</h1>
 
 @endsection
 
